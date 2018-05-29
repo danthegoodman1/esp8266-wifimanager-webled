ba363ad851a9b80b75a8e2a192f5515679dd3b78
danthegoodman1 <xxdanthegoodmanxx@gmail.com>
2018-05-28 23:10:13 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+let redC = 255;
+let blueC = 255;
+let greenC = 255;
+
+app.get("/", (req, res) => {
+    console.log("request!!!!!");
+    res.send("Hello");
+})
+.get("/getcolors", (req, res) => {
+    let colors = {
+        red: redC,
+        blue: blueC,
+        green: greenC
+    };
+
+    res.send(JSON.stringify(colors))
+})
+.post("/setcolors", (req, res) => {
+    redC = req.body.redColor;
+    console.log(`Set Red to: ${redC}`);
+    blueC = req.body.blueColor;
+    console.log(`Set Blue to: ${blueC}`);
+    greenC = req.body.greenColor;
+    console.log(`Set Green to: ${greenC}`);
+    res.send("Got colors");
+});
+
+app.listen(80, "0.0.0.0", () => {
+    console.log("listening!");
+    setInterval(function(){
+        console.log(`Current Colors: Red: ${redC}, Green: ${greenC}, Blue: ${blueC}`);
+    }, 2000);
+});
